3630fae6f34e1c92c9f1c2d8521a3e494d6d8b12
Matos <matos@localhost.localdomain>
2020-08-12 02:06:52 +0400

    Removed console.logs

--- src/app/shared/directives/block-copy-paste.directive.ts
+++ src/app/shared/directives/block-copy-paste.directive.ts
@@ -9,24 +9,16 @@ export class BlockCopyPasteDirective {
 
   @HostListener('paste', ['$event'])
   public blockPaste(e: KeyboardEvent):void {
-    console.log("hayop");
     e.preventDefault();
   }
 
-  @HostListener('click')
-  public nameF() {
-    console.log("ha");
-  };
-
   @HostListener('copy', ['$event'])
   public blockCopy(e: KeyboardEvent):void {
-    console.log("hayoc");
     e.preventDefault();
   }
 
   @HostListener('cut', ['$event'])
   public blockCut(e: KeyboardEvent):void {
-    console.log("hayocut");
     e.preventDefault();
   }
 
