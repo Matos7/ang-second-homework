5fddd8cda88f3cd4bf059fb7e33a92100ff9623d
Matos <matos@localhost.localdomain>
2020-08-20 23:55:13 +0400

    Moved products-list, separated routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ForgetPasswordComponent } from './pages/forget-password/forget-password.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'product-detail/:id', component: ProductDetailComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'forget-password', component: ForgetPasswordComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: NotFoundComponent }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports : [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,52 +1,31 @@
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './components/root/app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { ProductsListComponent } from './components/products-list/products-list.component';
-import { ProductRowComponent } from './components/products-list/product-row/product-row.component';
+import { ProductsListComponent } from './pages/products-list/products-list.component';
+import { ProductRowComponent } from './pages/products-list/product-row/product-row.component';
 import { ProvidersModule } from './core/providers.module';
 import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { LoginComponent } from './pages/login/login.component';
-import { ForgetPasswordComponent } from './pages/forget-password/forget-password.component';
-import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
-
-const appRoutes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'product-detail/:id', component: ProductDetailComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'forget-password', component: ForgetPasswordComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'contact', component: ContactComponent },
-  { path: '**', component: NotFoundComponent }
-];
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
     HeaderComponent,
     ProductsListComponent,
     ProductRowComponent,
     FooterComponent,
-    HomeComponent,
-    AboutComponent,
-    ContactComponent,
-    NotFoundComponent,
-    RegisterComponent,
-    LoginComponent,
-    ForgetPasswordComponent,
-    ProductDetailComponent,
   ],
   imports: [
     BrowserModule,
@@ -54,7 +33,8 @@ const appRoutes: Routes = [
     SharedModule,
     ProvidersModule,
     HttpClientModule,
-    RouterModule.forRoot(appRoutes)
+    ReactiveFormsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
